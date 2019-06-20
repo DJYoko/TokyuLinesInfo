947783f1b427c4d165c3b3d2b0968e2ae05b66a0
dj_yoko <yokochan.recurrent@gmail.com>
2019-06-21 00:41:50 +0900

    add simple data

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,31 @@
 <template>
-  <h1>Top</h1>
+  <div>
+    <h1>Top</h1>
+    <ul>
+      <li v-for="line in lines" :key="line.initial">
+    {{ line.name }}
+  </li>
+    </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  data: ()=> {
+    return {
+      lines: [
+        {
+          name: '東横線',
+          initial: 'TY',
+        }
+      ]
+    }
+  }
+  // fetch ({ store, params }) {
+  //   return axios.get('http://my-api/stars')
+  //     .then((res) => {
+  //       store.commit('setStars', res.data)
+  //     })
+  // }
+}
+</script>

b6508e39a5156aa16dfa59e5bae81f5dd42ee700
dj_yoko <yokochan.recurrent@gmail.com>
2019-06-22 01:03:17 +0900

    create Mainvisual, add photo of Mainvisual.

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,55 @@
 <template>
   <div>
-    <h1>Top</h1>
+    <div
+      id="mainvisual"
+      :style="{ backgroundImage: `url(${backgroundImageUrl})` }"
+    >
+      <div class="container">
+        <h1 class="text-center">Tokyu Lines info</h1>
+      </div>
+    </div>
     <ul>
-      <li v-for="line in lines" :key="line.initial">
-    {{ line.name }}
-  </li>
+      <li
+        v-for="line in lines"
+        :key="line.initial"
+      >
+        {{ line.name }}
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import backgroundImage from "~/assets/PAK86_touyokosentohome.jpg";
 export default {
-  data: ()=> {
+  data: () => {
     return {
-      lines: [
-        {
-          name: '東横線',
-          initial: 'TY',
-        }
-      ]
-    }
+      backgroundImageUrl: backgroundImage,
+      isLoading: true,
+      lines: []
+    };
+  },
+  mounted() {
+    console.log("run");
+    axios.get("lines.json").then(response => {
+      this.lines = response.data;
+    });
   }
-  // fetch ({ store, params }) {
-  //   return axios.get('http://my-api/stars')
-  //     .then((res) => {
-  //       store.commit('setStars', res.data)
-  //     })
-  // }
-}
+};
 </script>
+
+<style scoped lang="scss">
+#mainvisual {
+  position: relative;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  h1 {
+    padding:240px 0;
+    color:#ffffff;
+    font-weight: 100;
+    font-size:5rem;
+  }
+}
+</style>

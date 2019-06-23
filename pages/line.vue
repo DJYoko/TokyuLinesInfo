bcc3fd24173e23c240db88fee59e260bc91cf9e2
dj_yoko <yokochan.recurrent@gmail.com>
2019-06-24 00:15:20 +0900

    add initial method on Line page. (load static JSON & parse)

--- pages/line.vue
+++ pages/line.vue
@@ -13,28 +13,19 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data: () => {
     return {
-      lineStations: [
-        {
-          name: "渋谷",
-          id: "TY01",
-          station_id: "00001"
-        },
-        {
-          name: "横浜",
-          id: "TY20",
-          station_id: "10001"
-        }
-      ]
+      lineStations: []
     };
+  },
+  mounted() {
+    const lineId = this.$route.query.id;
+    axios.get(`./lineStation/${lineId}.json`).then(response => {
+      console.log(response);
+      this.lineStations = response.data;
+    });
   }
-  // fetch ({ store, params }) {
-  //   return axios.get('http://my-api/stars')
-  //     .then((res) => {
-  //       store.commit('setStars', res.data)
-  //     })
-  // }
 };
 </script>

9ab6a5f03e8d8d63a2752ada7643366cf3665bd9
dj_yoko <yokochan.recurrent@gmail.com>
2019-06-25 00:32:36 +0900

    add header style of LINE detail page

--- pages/line.vue
+++ pages/line.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
-    <h1>Line</h1>
+    <div class="line-title-block">
+      <div
+        class="line-symbol"
+        :style="symbolStyle"
+      >
+        {{ displayLabel }}
+      </div>
+      <h1>
+        {{ line.name }}
+      </h1>
+    </div>
+
     <ul>
       <li
         v-for="lineStation in lineStations"
@@ -14,10 +25,17 @@
 
 <script>
 import axios from "axios";
+import util from "~/plugins/util";
 export default {
   data: () => {
     return {
-      lineStations: []
+      lineStations: [],
+      line: {
+        backgroundColor: "",
+        id: "",
+        name: "",
+        textColor: ""
+      }
     };
   },
   mounted() {
@@ -26,6 +44,51 @@ export default {
       console.log(response);
       this.lineStations = response.data;
     });
+
+    axios.get(`./lines.json`).then(response => {
+      console.log(response.data, lineId);
+      this.line = util.getObjectById(response.data, lineId);
+
+      console.log(this.line);
+    });
+  },
+  computed: {
+    displayLabel() {
+      return this.line.id.toUpperCase();
+    },
+    symbolStyle() {
+      return {
+        backgroundColor: this.line.backgroundColor,
+        color: this.line.textColor
+      };
+    }
   }
 };
 </script>
+
+
+<style scoped lang="scss">
+@import "~assets/variables";
+.line-title-block {
+  position: relative;
+  height: $itemMargin * 4;
+}
+.line-symbol {
+  position: absolute;
+  left: $itemMargin;
+  top: $itemMargin;
+  width: $itemMargin * 2;
+  height: $itemMargin * 2;
+  line-height: $itemMargin * 2;
+  border-radius: $itemRadius;
+  text-align: center;
+}
+
+h1 {
+  font-size: $itemMargin * 2;
+  height: $itemMargin * 4;
+  line-height: $itemMargin * 4;
+  margin-bottom: 0;
+  padding: 0 $itemMargin 0 $itemMargin * 5;
+}
+</style>

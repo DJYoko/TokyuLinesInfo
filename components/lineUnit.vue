3aa4dccc406a804f5a7f732558e3119271e42372
dj_yoko <yokochan.recurrent@gmail.com>
2019-07-02 00:32:13 +0900

    add border on lineUnit

--- components/lineUnit.vue
+++ components/lineUnit.vue
@@ -2,6 +2,7 @@
   <div
     class="line-unit"
     @click="onClick"
+    :style="borderStyle"
   >
     <div
       class="color-label"
@@ -32,6 +33,11 @@ export default {
     },
     displayLabel() {
       return this.id.toUpperCase();
+    },
+    borderStyle() {
+      return {
+        border: `1px solid ${this.backgroundColor}`
+      };
     }
   },
   methods: {
@@ -47,28 +53,26 @@ export default {
 .line-unit {
   text-align: center;
   cursor: pointer;
+  border-radius: $itemRadius;
+  transition: $transition;
 }
 
 .color-label {
   padding: $itemMargin 0;
   font-weight: bold;
   border-radius: $itemRadius $itemRadius 0 0;
-  transition: $transition;
 }
 .detail-name {
   font-size: 0.8em;
   padding: $itemMargin 0;
   background-color: #ffffff;
   border-radius: 0 0 $itemRadius $itemRadius;
-  transition: $transition;
 }
 
 .line-unit {
   &:hover {
-    .color-label,
-    .detail-name {
-      transform: scale(1.1);
-    }
+    text-decoration: none;
+    transform: scale(1.1);
   }
 }
 </style>

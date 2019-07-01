6f375f76914befc2911afead957c0b113dc4e4c9
dj_yoko <yokochan.recurrent@gmail.com>
2019-07-02 00:39:18 +0900

    add BackLink component

---
+++ components/backLink.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="back-link-wrap">
+      <nuxt-link class="back-link" :to="{path: '/'}">back to home</nuxt-link>
+</div>
+</template>
+
+<script>
+export default {
+  name: "backLink",
+};
+</script>
+
+<style scoped lang="scss">
+@import "~assets/variables";
+.back-link-wrap{
+  background:#111;
+}
+</style>

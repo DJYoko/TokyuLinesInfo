9afe7605f3dda619093e96b3e085a858693455f0
dj_yoko <yokochan.recurrent@gmail.com>
2019-07-03 00:31:54 +0900

    add config for Github-Page deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ const routerBase =
   process.env.DEPLOY_ENV === 'GH_PAGES'
     ? {
         router: {
-          base: '/djyoko.github.io/TokyuLinesInfo/',
+          base: '/TokyuLinesInfo/',
         },
       }
     : {}

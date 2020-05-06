6d6dd7884a9d6caf55f53eab0aa1320ad50d18be
DJYoko <yokochan.recurrent@gmail.com>
2020-05-06 12:03:38 +0900

    update binary

---
+++ pages/amp_static/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>pages\amp\dummy\index.vue
+    <br> this page is for only Generate AMP HTML
+  </div>
+</template>
+
+<script>
+export default {
+  amp: 'only',
+  ampLayout: 'default.amp',
+};
+</script>

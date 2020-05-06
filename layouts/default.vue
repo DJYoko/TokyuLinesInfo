68c1cb2d34641cf2aae22212e46dc4ce378126ac
DJYoko <yokochan.recurrent@gmail.com>
2020-05-06 11:29:32 +0900

    [AMP] loac external CSS (Bootstrap)

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,10 @@ export default {
         }
     },
     head () {
-         const link = []
+        const link = [
+            { rel: 'stylesheet', href: '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' },
+            { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css', integrity: 'sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm', crossorigin:'anonymous' }
+        ]
         if(this.$props.hasAmpPage) {
             const routePath = `/amp${this.$route.path}`.replace(/\/$/g, '')
             const ampCanonicalLink = { rel: 'amphtml', href: routePath}

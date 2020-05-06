7b24235254b1921d8fe69c3d0bb97f87fb061dd8
DJYoko <yokochan.recurrent@gmail.com>
2020-05-06 12:02:18 +0900

    [AMP] remove custom canonical settings

--- layouts/default.amp.vue
+++ layouts/default.amp.vue
@@ -5,10 +5,8 @@
 export default {
     head () {
       // canonical link to original page
-        const routePath = this.$route.path.replace(/^\/amp/, '/')
         const link = [
             { rel: 'preload', as:'script', href:'https://cdn.ampproject.org/v0.js' },
-            { rel: 'canonical', href: routePath},
         ]
         return {
             link

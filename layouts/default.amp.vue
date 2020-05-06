42fff67f7c1b527a2e4bf9976a5164b4811f218f
DJYoko <yokochan.recurrent@gmail.com>
2020-05-06 11:22:32 +0900

    [AMP] add layout(s) for both of AMP and Default

---
+++ layouts/default.amp.vue
@@ -0,0 +1,18 @@
+<template>
+  <Nuxt />
+</template>
+<script>
+export default {
+    head () {
+      // canonical link to original page
+        const routePath = this.$route.path.replace(/^\/amp/, '/')
+        const link = [
+            { rel: 'preload', as:'script', href:'https://cdn.ampproject.org/v0.js' },
+            { rel: 'canonical', href: routePath},
+        ]
+        return {
+            link
+        }
+    }
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+    <nuxt/>
+</template>
+<script>
+export default {
+    props: {
+        hasAmpPage: {
+            type: Boolean,
+            default: true
+        }
+    },
+    head () {
+         const link = []
+        if(this.$props.hasAmpPage) {
+            const routePath = `/amp${this.$route.path}`.replace(/\/$/g, '')
+            const ampCanonicalLink = { rel: 'amphtml', href: routePath}
+            link.push(ampCanonicalLink)
+        }
+        return {
+            link
+        }
+    },
+}
+</script>
